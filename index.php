d800da3cf8feee293b1c9fbb83a6ab3f1330fb52
justinhunt <bitwalkerjapan@gmail.com>
2020-10-25 15:58:18 +0900

    initial commit of plugin

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * List the tool provided in a course
+ *
+ * @package    enrol_poodllprovider
+ * @copyright  2020 Justin Hunt <justin@poodll.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../config.php');
+require_once($CFG->dirroot.'/enrol/poodllprovider/lib.php');
+
+$courseid = required_param('courseid', PARAM_INT);
+$action = optional_param('action', '', PARAM_ALPHA);
+if ($action) {
+    require_sesskey();
+    $instanceid = required_param('instanceid', PARAM_INT);
+    $instance = $DB->get_record('enrol', array('id' => $instanceid), '*', MUST_EXIST);
+}
+$confirm = optional_param('confirm', 0, PARAM_INT);
+
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+
+$context = context_course::instance($course->id);
+
+require_login($course);
+require_capability('moodle/course:enrolreview', $context);
+
+$poodllproviderplugin = enrol_get_plugin('poodllprovider');
+$canconfig = has_capability('moodle/course:enrolconfig', $context);
+$pageurl = new moodle_url('/enrol/poodllprovider/index.php', array('courseid' => $courseid));
+
+$PAGE->set_url($pageurl);
+$PAGE->set_title(get_string('course') . ': ' . $course->fullname);
+$PAGE->set_pagelayout('admin');
+
+// Check if we want to perform any actions.
+if ($action) {
+    if ($action === 'delete') {
+        if ($poodllproviderplugin->can_delete_instance($instance)) {
+            if ($confirm) {
+                $poodllproviderplugin->delete_instance($instance);
+                redirect($PAGE->url);
+            }
+
+            $yesurl = new moodle_url('/enrol/poodllprovider/index.php',
+                array('courseid' => $course->id,
+                    'action' => 'delete',
+                    'instanceid' => $instance->id,
+                    'confirm' => 1,
+                    'sesskey' => sesskey())
+                );
+            $displayname = $poodllproviderplugin->get_instance_name($instance);
+            $users = $DB->count_records('user_enrolments', array('enrolid' => $instance->id));
+            if ($users) {
+                $message = markdown_to_html(get_string('deleteinstanceconfirm', 'enrol',
+                    array('name' => $displayname,
+                          'users' => $users)));
+            } else {
+                $message = markdown_to_html(get_string('deleteinstancenousersconfirm', 'enrol',
+                    array('name' => $displayname)));
+            }
+            echo $OUTPUT->header();
+            echo $OUTPUT->confirm($message, $yesurl, $PAGE->url);
+            echo $OUTPUT->footer();
+            die();
+        }
+    } else if ($action === 'disable') {
+        if ($poodllproviderplugin->can_hide_show_instance($instance)) {
+            if ($instance->status != ENROL_INSTANCE_DISABLED) {
+                $poodllproviderplugin->update_status($instance, ENROL_INSTANCE_DISABLED);
+                redirect($PAGE->url);
+            }
+        }
+    } else if ($action === 'enable') {
+        if ($poodllproviderplugin->can_hide_show_instance($instance)) {
+            if ($instance->status != ENROL_INSTANCE_ENABLED) {
+                $poodllproviderplugin->update_status($instance, ENROL_INSTANCE_ENABLED);
+                redirect($PAGE->url);
+            }
+        }
+    }
+}
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('toolsprovided', 'enrol_poodllprovider'));
+echo "<p>" .get_string('toolsprovided_help', 'enrol_poodllprovider') . "</p>";
+echo "<p class=helplink>" . $OUTPUT->doc_link('enrol/poodllprovider/index',
+    get_string('morehelp')) ."</p>";
+
+if (\enrol_poodllprovider\helper::count_poodllprovider_tools(array('courseid' => $courseid)) > 0) {
+    $table = new \enrol_poodllprovider\manage_table($courseid);
+    $table->define_baseurl($pageurl);
+    $table->out(50, false);
+} else {
+    $notify = new \core\output\notification(get_string('notoolsprovided', 'enrol_poodllprovider'),
+        \core\output\notification::NOTIFY_WARNING);
+    echo $OUTPUT->render($notify);
+}
+
+if ($poodllproviderplugin->can_add_instance($course->id)) {
+    echo $OUTPUT->single_button(new moodle_url('/enrol/editinstance.php',
+        array(
+            'type' => 'poodllprovider',
+            'courseid' => $course->id,
+            'returnurl' => new moodle_url('/enrol/poodllprovider/index.php', array('courseid' => $course->id)))
+        ),
+        get_string('add'));
+}
+
+echo $OUTPUT->footer();
